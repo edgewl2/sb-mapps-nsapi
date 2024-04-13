0c21f1d1e37917695c73564824ce37612ea6f3ab
Gerardo Hernandez <edgewl2@gmail.com>
2024-04-13 13:02:04 -0600

    feat(project): initial Commit

---
+++ src/main/java/ni/dev/edgeahz/nsapi/DemoApplication.java
@@ -0,0 +1,13 @@
+package ni.dev.edgeahz.nsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ni/dev/edgeahz/nsapi/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ni.dev.edgeahz.nsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
